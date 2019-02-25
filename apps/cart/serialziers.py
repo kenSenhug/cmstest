85d7abda8196dc91f48e3bc83a1ceb84a5335411
lishichao <chao@qq.com>
2019-02-25 17:20:51 +0800

    购物车增删改查

--- apps/cart/serialziers.py
+++ apps/cart/serialziers.py
@@ -19,3 +19,29 @@ class CartAddSerializer(serializers.Serializer):
             # raise ValidationError({'message': '商品不存在'})          # ok
             # return Response({'message': '商品不存在'}, status=400)   # error
         return value
+
+
+class CartSKUSerializer(serializers.ModelSerializer):
+    """
+    查询购物车商品使用到的序列化器
+    """
+    count = serializers.IntegerField(label='数量')
+    selected = serializers.BooleanField(label='是否勾选')
+
+    class Meta:
+        model = Goods
+        fields = ('id', 'title', 'img_url', 'sell_price', 'count', 'selected')
+
+
+class CartDeleteSerializer(serializers.Serializer):
+    """
+    删除购物车数据序列化器
+    """
+    sku_id = serializers.IntegerField(label='商品id', min_value=1)
+
+    def validate_sku_id(self, value):
+        try:
+            sku = Goods.objects.get(id=value)
+        except Goods.DoesNotExist:
+            raise serializers.ValidationError('商品不存在')
+        return value
